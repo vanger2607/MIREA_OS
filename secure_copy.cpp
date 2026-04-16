3856b5af8c8ac50eb6305bc3c3f6addfd198b1bf
vanger2607 <vanger2607@gmail.com>
2026-04-16 11:45:02 +0300

    delete pools

--- secure_copy.cpp
+++ secure_copy.cpp
@@ -14,7 +14,6 @@
 #include <sstream>
 #include <sys/stat.h>
 
-#include "readerwriterqueue.h" 
 #include "caesar.h"
 
 const size_t BLOCK_SIZE = 4096;
@@ -26,14 +25,9 @@ void handle_sigint(int sig) {
     keep_running = 0; 
 }
 
-struct DataBlock {
-    unsigned char* ptr;
-    size_t size;
-};
-
 /**
  * ГЛОБАЛЬНЫЙ КОНТЕКСТ:
- * Общие данные для 3-х главных потоков.
+ * Общие данные для главных потоков.
  */
 struct GlobalContext {
     std::vector<std::string> input_files;
@@ -51,80 +45,6 @@ struct GlobalContext {
         if (log_file.is_open()) log_file.close();
     }
 };
-
-/**
- * СТРУКТУРА КОНТЕКСТА:
- * Мы упаковываем всё, что нужно потокам, в одну структуру.
- */
-struct AppContext {
-    moodycamel::ReaderWriterQueue<DataBlock> queue;
-    std::atomic<bool> producer_finished;
-    char key;
-    std::string input_filename;
-    std::string output_filename;
-
-    // Конструктор для удобной инициализации
-    AppContext(char k, const std::string& in, const std::string& out) 
-        : queue(128), producer_finished(false), key(k), input_filename(in), output_filename(out) {}
-};
-
-/**
- * ПОТОК 1: PRODUCER
- */
-void* producer_func(void* arg) {
-    // Распаковываем контекст
-    AppContext* ctx = static_cast<AppContext*>(arg);
-    // Передаем ключ в библиотеку
-    set_key(ctx->key);
-
-    std::ifstream infile(ctx->input_filename, std::ios::binary);
-
-    while (keep_running && infile) {
-      // Создаем буффер (фиксируем длину сообщения)
-        unsigned char* buffer = new (std::nothrow) unsigned char[BLOCK_SIZE];
-        if (!buffer) break;
-        // пытаемся прочитать блок в наш буффер
-        infile.read(reinterpret_cast<char*>(buffer), BLOCK_SIZE);
-        // смотрим сколько реально прочитали
-        size_t read_bytes = infile.gcount();
-
-        if (read_bytes > 0) {
-            caesar(buffer, buffer, (int)read_bytes);
-            ctx->queue.enqueue({buffer, read_bytes});
-        } else {
-            delete[] buffer;
-            if (infile.eof()) break;
-        }
-    }
-   // Сообщаем второму потоку, что новых данных больше не будет 
-    ctx->producer_finished.store(true, std::memory_order_release);
-    return nullptr;
-}
-
-/**
- * ПОТОК 2: CONSUMER
- */
-void* consumer_func(void* arg) {
-    AppContext* ctx = static_cast<AppContext*>(arg);
-    std::ofstream outfile(ctx->output_filename, std::ios::binary);
-
-    while (true) {
-        DataBlock block;
-        if (ctx->queue.try_dequeue(block)) {
-            outfile.write(reinterpret_cast<char*>(block.ptr), block.size);
-            // пишем ровно столько байт, сколько было прочитано
-            delete[] block.ptr;
-            // освобождаем память выделенную первым потоком
-        } else { // Если нет данных, то проверяем не закончил ли первый поток передачу
-            if (ctx->producer_finished.load(std::memory_order_acquire)) {
-                break;
-            }
-            // Очередь пуста, делать пока нечего, поэтому говорим процессору, что бездельничаем, другие могут работать
-            sched_yield();
-        }
-    }
-    return nullptr;
-}
 
 // Потокобезопасное логирование с try_lock
 void log_message(GlobalContext* gctx, const std::string& filename) {
@@ -155,13 +75,15 @@ void log_message(GlobalContext* gctx, const std::string& filename) {
  */
 void* worker_func(void* arg) {
     GlobalContext* gctx = static_cast<GlobalContext*>(arg);
+    // Передаем ключ в библиотеку
+    set_key(gctx->key);
 
     while (keep_running) {
         size_t idx = gctx->file_idx.fetch_add(1, std::memory_order_relaxed);
         if (idx >= gctx->input_files.size()) {
             break;
         }
-	std::string filepath = gctx->input_files[idx];
+        std::string filepath = gctx->input_files[idx];
         std::string filename = filepath;
         size_t slash_pos = filepath.find_last_of("/\\");
         if (slash_pos != std::string::npos) {
@@ -169,18 +91,24 @@ void* worker_func(void* arg) {
         }
         std::string out_filepath = gctx->out_dir + "/" + filename;
 
-        // Создаем объект контекста
-        AppContext ctx(gctx->key, filepath, out_filepath);
+        std::ifstream infile(filepath, std::ios::binary);
+        std::ofstream outfile(out_filepath, std::ios::binary);
 
-        pthread_t t1, t2;
-
-        // Передаем АДРЕС структуры ctx в оба потока и создаём их
-        pthread_create(&t1, nullptr, producer_func, &ctx);
-        pthread_create(&t2, nullptr, consumer_func, &ctx);
-        
-        // Ждем окончания потоков, чтобы убить зомби
-        pthread_join(t1, nullptr);
-        pthread_join(t2, nullptr);
+        if (infile && outfile) {
+            // Создаем буффер (фиксируем длину сообщения)
+            unsigned char buffer[BLOCK_SIZE];
+            while (keep_running && infile) {
+                // пытаемся прочитать блок в наш буффер
+                infile.read(reinterpret_cast<char*>(buffer), BLOCK_SIZE);
+                // смотрим сколько реально прочитали
+                size_t read_bytes = infile.gcount();
+                if (read_bytes > 0) {
+                    caesar(buffer, buffer, (int)read_bytes);
+                    // пишем ровно столько байт, сколько было прочитано
+                    outfile.write(reinterpret_cast<char*>(buffer), read_bytes);
+                }
+            }
+        }
 
         if (keep_running) {
             log_message(gctx, filename);
@@ -209,7 +137,6 @@ int main(int argc, char* argv[]) {
 
     const int NUM_THREADS = 3;
     pthread_t workers[NUM_THREADS];
-
     for (int i = 0; i < NUM_THREADS; ++i) {
         pthread_create(&workers[i], nullptr, worker_func, &gctx);
     }
